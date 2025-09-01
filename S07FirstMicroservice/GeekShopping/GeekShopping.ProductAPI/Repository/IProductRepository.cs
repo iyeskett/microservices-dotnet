9ee84cc0e663368a08637334feb4c932a2ebdf1f
Felipe Mello <fmello@noovi.com.br>
2025-09-01 13:36:29 -0300

    Criação de métodos do IProductRepository

--- S07FirstMicroservice/GeekShopping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
+++ S07FirstMicroservice/GeekShopping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace GeekShopping.ProductAPI.Repository
+﻿using GeekShopping.ProductAPI.Data.DTO;
+
+namespace GeekShopping.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDTO>> FindAll();
+
+        Task<ProductDTO> FindById(long id);
+
+        Task<ProductDTO> Create(ProductDTO product);
+
+        Task<ProductDTO> Update(ProductDTO product);
+
+        Task<bool> Delete(long id);
     }
 }
