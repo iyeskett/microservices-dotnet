437ac31f22f64a4b965f82b9791a322175c1aa61
Felipe Mello <fmello@noovi.com.br>
2025-09-01 17:14:47 -0300

    Front end: Criação da interface do serviço para consumir a ProductAPI

---
+++ S07FirstMicroservice/GeekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> FindAllProducts();
+
+        Task<Product> FindProductById(long id);
+
+        Task<Product> CreateProduct(Product product);
+
+        Task<Product> UpdateProduct(Product product);
+
+        Task<bool> DeleteProductById(long id);
+    }
+}
