29fad20f94f12f0bb79c7c3dcdce76dff38710c6
Felipe Mello <fmello@noovi.com.br>
2025-09-01 15:43:31 -0300

    Implementação do controller de Product com Get

---
+++ S07FirstMicroservice/GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using GeekShopping.ProductAPI.Data.DTO;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> FindAll()
+        {
+            return Ok(await _productRepository.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDTO>> FindById(long id)
+        {
+            var product = await _productRepository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+    }
+}
