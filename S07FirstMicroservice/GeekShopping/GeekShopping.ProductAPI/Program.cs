5c9a59a49ca47105c4c8d924439ee5fc75645d14
Felipe Mello <fmello@noovi.com.br>
2025-09-01 14:20:20 -0300

    Implementação do ProductRepository

--- S07FirstMicroservice/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ S07FirstMicroservice/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,4 +1,5 @@
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,7 @@ builder.Services.AddSwaggerGen();
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 37))));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
