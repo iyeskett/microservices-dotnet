c052e4997485bc5e5dc51f0c561490412275652c
Felipe Mello <fmello@noovi.com.br>
2025-09-04 14:34:50 -0300

    Fron end: Adicionando configurações de segurança no Program.cs

--- S07FirstMicroservice/GeekShopping/GeekShopping.Web/Program.cs
+++ S07FirstMicroservice/GeekShopping/GeekShopping.Web/Program.cs
@@ -1,5 +1,6 @@
 using GeekShopping.Web.Services;
 using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,27 @@ builder.Services.AddHttpClient<IProductService, ProductService>(_ =>
     _.BaseAddress = new Uri(builder.Configuration["ServiceURLs:ProductAPI"]);
 });
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(_ =>
+{
+    _.DefaultScheme = "Cookies";
+    _.DefaultChallengeScheme = "oidc";
+})
+    .AddCookie("Cookies", _ => _.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+    .AddOpenIdConnect("oidc", _ =>
+    {
+        _.Authority = builder.Configuration["ServiceURLs:IdentityServer"];
+        _.GetClaimsFromUserInfoEndpoint = true;
+        _.ClientId = "geek_shopping";
+        _.ClientSecret = "teste";
+        _.ResponseType = "code";
+        _.ClaimActions.MapJsonKey("role", "role", "role");
+        _.ClaimActions.MapJsonKey("sub", "sub", "sub");
+        _.TokenValidationParameters.NameClaimType = "name";
+        _.TokenValidationParameters.RoleClaimType = "role";
+        _.Scope.Add("geek_shopping");
+        _.SaveTokens = true;
+    });
 
 var app = builder.Build();
 
@@ -22,6 +44,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
