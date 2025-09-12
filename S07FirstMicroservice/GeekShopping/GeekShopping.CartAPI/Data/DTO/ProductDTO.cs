75361bdf08e866967a1a0f6d5e0111c5fec8d187
Felipe Mello <fmello@noovi.com.br>
2025-09-11 22:32:43 -0300

    CartAPI: Configurando Program.cs

---
+++ S07FirstMicroservice/GeekShopping/GeekShopping.CartAPI/Data/DTO/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace GeekShopping.CartAPI.Data.DTO
+{
+    public class ProductDTO
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public string CategoryName { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
---
+++ S07FirstMicroservice/GeekShopping/GeekShopping.CartAPI/Model/Context/MySqlContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+        }
+
+        protected MySqlContext()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
