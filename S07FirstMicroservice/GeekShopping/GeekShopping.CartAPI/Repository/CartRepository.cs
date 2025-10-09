30892442d5ef4a49a8f5ddadf90463812579f761
Felipe Mello <felipesilva.mello27@gmail.com>
2025-10-09 14:21:21 -0300

    CartAPI: Criando repositório

---
+++ S07FirstMicroservice/GeekShopping/GeekShopping.CartAPI/Repository/CartRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.DTO;
+using GeekShopping.CartAPI.Model.Context;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly MySqlContext _context;
+        private IMapper _mapper;
+
+        public CartRepository(MySqlContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<bool> ApplyCoupon(string userId, string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDTO> FindCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCoupon(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(long cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDTO> SaveOrUpdateCart(CartDTO cartDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
