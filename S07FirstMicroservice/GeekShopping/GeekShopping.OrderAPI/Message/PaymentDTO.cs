a0354c3bd4eaca0028c060b827d990cd18dd7c78
Felipe Mello <felipesilva.mello27@gmail.com>
2025-10-23 17:46:15 -0300

    PaymentProcessor: Criando classes message

---
+++ S07FirstMicroservice/GeekShopping/GeekShopping.OrderAPI/Message/PaymentDTO.cs
@@ -0,0 +1,15 @@
+﻿using GeekShopping.MessageBus;
+
+namespace GeekShopping.OrderAPI.Message
+{
+    public class PaymentDTO : BaseMessage
+    {
+        public long OrderId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string CVV { get; set; } = string.Empty;
+        public string ExpireMonthYear { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public decimal PurchaseAmount { get; set; }
+    }
+}
