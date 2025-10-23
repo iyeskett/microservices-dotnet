9c347f50bf28d6c585112efe0ef7b10a14a07f07
Felipe Mello <felipesilva.mello27@gmail.com>
2025-10-23 13:08:55 -0300

    OOrderAPI: Criando classes para o RabbitMQ

---
+++ S07FirstMicroservice/GeekShopping/GeekShopping.OrderAPI/Message/CartDetailDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace GeekShopping.OrderAPI.Message
+{
+    public class CartDetailDTO
+    {
+        public long Id { get; set; }
+        public long CartHeaderId { get; set; }
+
+        public long ProductId { get; set; }
+
+        public virtual ProductDTO? Product { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ S07FirstMicroservice/GeekShopping/GeekShopping.OrderAPI/Message/CheckoutDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace GeekShopping.OrderAPI.Message
+{
+    public class CheckoutDTO
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string CouponCode { get; set; } = string.Empty;
+        public decimal PurchaseAmount { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime DateTime { get; set; }
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+        public string CVV { get; set; } = string.Empty;
+        public string ExpiryMothYear { get; set; } = string.Empty;
+        public int CartTotalItems { get; set; }
+        public IEnumerable<CartDetailDTO> CartDetailDTOs { get; set; } = new List<CartDetailDTO>();
+    }
+}
