16c7046d3dc0dcf5da4432d9af8eb6360bd8d444
Felipe Mello <felipesilva.mello27@gmail.com>
2025-10-23 13:01:17 -0300

    OrderAPI: Criando classe repositório

---
+++ S07FirstMicroservice/GeekShopping/GeekShopping.OrderAPI/Repository/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using GeekShopping.OrderAPI.Model;
+
+namespace GeekShopping.OrderAPI.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<bool> AddOrder(OrderHeader orderHeader);
+
+        Task<bool> UpdateOrderPaymentStatus(long orderHeaderId, bool paid);
+    }
+}
---
+++ S07FirstMicroservice/GeekShopping/GeekShopping.OrderAPI/Repository/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using GeekShopping.OrderAPI.Model;
+using GeekShopping.OrderAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.OrderAPI.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DbContextOptions<MySqlContext> _context;
+
+        public OrderRepository(DbContextOptions<MySqlContext> context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddOrder(OrderHeader orderHeader)
+        {
+            if (orderHeader == null) return false;
+            await using var _db = new MySqlContext(_context);
+            _db.OrderHeaders.Add(orderHeader);
+            await _db.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> UpdateOrderPaymentStatus(long orderHeaderId, bool paid)
+        {
+            await using var _db = new MySqlContext(_context);
+            var orderHeader = await _db.OrderHeaders.FirstOrDefaultAsync(_ => _.Id == orderHeaderId);
+            if (orderHeader == null) return false;
+            orderHeader.PaymentStatus = paid;
+            _db.OrderHeaders.Update(orderHeader);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+    }
+}
